a8840ef4c191a80ce2aa7565750d84cb93d9b8e5
donchuks <chukwudidavid02@gmail.com>
2025-01-23 07:54:08 +0100

    created a user profile model and serializer

--- encryption_tool/apps.py
+++ encryption_tool/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class EncryptionToolConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'encryption_tool'
+
+def ready(self):
+    import encryption_tool.signals
